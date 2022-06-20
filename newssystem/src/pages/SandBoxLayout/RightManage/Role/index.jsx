83251f5e6b9222b4e4e9d47857fb600b77310f51
CrazyCatZhang <CrazyCatZhang@outlook.com>
2022-06-20 16:27:58 +0800

    Complete the permission control of the role list

--- newssystem/src/pages/SandBoxLayout/RightManage/Role/index.jsx
+++ newssystem/src/pages/SandBoxLayout/RightManage/Role/index.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import {Button, Modal, Table} from "antd";
+import {Button, Modal, Table, Tree} from "antd";
 import {DeleteOutlined, EditOutlined, ExclamationCircleOutlined} from "@ant-design/icons";
 
 const {confirm} = Modal
@@ -9,6 +9,10 @@ const {confirm} = Modal
 function RoleList(props) {
 
     const [dataSource, setDataSource] = useState([])
+    const [isModalVisible, setIsModalVisible] = useState(false)
+    const [rightList, setRightList] = useState([])
+    const [currentRightList, setCurrentRightList] = useState([])
+    const [currentID, setCurrentID] = useState(0)
 
     const columns = [
         {
@@ -27,7 +31,11 @@ function RoleList(props) {
             render: (item) => {
                 return <div>
                     <Button danger shape="circle" icon={<DeleteOutlined/>} onClick={() => confirmMethod(item)}/>
-                    <Button type="primary" shape="circle" icon={<EditOutlined/>}/>
+                    <Button type="primary" shape="circle" icon={<EditOutlined/>} onClick={() => {
+                        setIsModalVisible(true)
+                        setCurrentRightList(item.rights)
+                        setCurrentID(item.id)
+                    }}/>
                 </div>
             }
         }
@@ -55,14 +63,59 @@ function RoleList(props) {
 
     useEffect(() => {
         axios.get('http://localhost:5001/roles').then(res => {
-            console.log(res.data)
             setDataSource(res.data)
         })
+        axios.get('http://localhost:5001/rights?_embed=children').then(res => {
+            let results = res.data.map(item => {
+                if (item.children?.length > 0) {
+                    item.children.forEach(child => {
+                        child.title = child.label
+                    })
+                }
+                item.title = item.label
+                return item
+            })
+            setRightList(results)
+        })
     }, [])
+
+    const handleOk = () => {
+        setIsModalVisible(false)
+        setDataSource(dataSource.map(item => {
+            if (item.id === currentID) {
+                return {
+                    ...item,
+                    rights: currentRightList
+                }
+            }
+            return item
+        }))
+        axios.patch(`http://localhost:5001/roles/${currentID}`, {
+            rights: currentRightList
+        })
+    }
+
+    const handleCancel = () => {
+        setIsModalVisible(false)
+    }
+
+    const onCheck = (checkedKeys) => {
+        setCurrentRightList(checkedKeys.checked)
+    }
 
     return (
         <div>
             <Table dataSource={dataSource} columns={columns} rowKey={(item) => item.id}></Table>
+            <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                <Tree
+                    checkable
+                    treeData={rightList}
+                    checkedKeys={currentRightList}
+                    onCheck={onCheck}
+                    checkStrictly={true}
+                >
+                </Tree>
+            </Modal>
         </div>
     );
 }
