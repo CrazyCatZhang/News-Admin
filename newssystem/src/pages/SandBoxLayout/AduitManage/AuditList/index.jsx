c1420c3f4db76f6437facda28369b7408e8d8685
CrazyCatZhang <CrazyCatZhang@outlook.com>
2022-06-29 21:42:52 +0800

    Revocation, Update and Release of Review Lists

--- newssystem/src/pages/SandBoxLayout/AduitManage/AuditList/index.jsx
+++ newssystem/src/pages/SandBoxLayout/AduitManage/AuditList/index.jsx
@@ -1,17 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import {useAuth} from "../../../../guard/AuthProvider";
 import axios from "axios";
-import {Button, Table, Tag} from "antd";
-import {Link} from "react-router-dom";
+import {Button, notification, Table, Tag} from "antd";
+import {Link, useNavigate, useParams} from "react-router-dom";
 
 function AuditList(props) {
 
     const [dataSource, setDataSource] = useState([])
 
     const {user: {username}} = useAuth()
+    const navigate = useNavigate()
 
     useEffect(() => {
-        axios.get(`/news?auth=${username}&auditState_ne=0&publishState_lte=1&_expand=category`).then(res => {
+        axios.get(`/news?author=${username}&auditState_ne=0&publishState_lte=1&_expand=category`).then(res => {
             setDataSource(res.data)
         })
     }, [username])
@@ -49,18 +50,47 @@ function AuditList(props) {
             render: (item) => {
                 return <div>
                     {
-                        item.auditState === 1 && <Button>撤销</Button>
+                        item.auditState === 1 && <Button onClick={() => handleRevert(item)}>撤销</Button>
                     }
                     {
-                        item.auditState === 2 && <Button danger>发布</Button>
+                        item.auditState === 2 && <Button danger onClick={() => handlePublish(item)}>发布</Button>
                     }
                     {
-                        item.auditState === 3 && <Button type="primary">更新</Button>
+                        item.auditState === 3 &&
+                        <Button type="primary" onClick={() => navigate(`/news-manage/update/${item.id}`)}>更新</Button>
                     }
                 </div>
             }
         }
     ];
+
+    const handleRevert = (item) => {
+        setDataSource(dataSource.filter(data => data.id !== item.id))
+        axios.patch(`/news/${item.id}`, {
+            "auditState": 0
+        }).then(res => {
+            notification.info({
+                message: `通知`,
+                description:
+                    `您可以到草稿箱中查看您的新闻`,
+                placement: 'bottomRight',
+            });
+        })
+    }
+
+    const handlePublish = (item) => {
+        axios.patch(`/news/${item.id}`, {
+            "publishState": 2
+        }).then(res => {
+            navigate(`/publish-manage/published`)
+            notification.info({
+                message: `通知`,
+                description:
+                    `您可以到【发布管理/已发布】中查看您的新闻`,
+                placement: 'bottomRight',
+            });
+        })
+    }
 
     return (
         <div>
