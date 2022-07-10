dcdb58225d69204597aa27f8a628b40c19595755
CrazyCatZhang <CrazyCatZhang@outlook.com>
2022-07-10 21:13:52 +0800

    Complete homepage display and layout

--- newssystem/src/pages/SandBoxLayout/Home/index.jsx
+++ newssystem/src/pages/SandBoxLayout/Home/index.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import {Avatar, Card, Col, List, Row} from "antd";
+import {EditOutlined, EllipsisOutlined, SettingOutlined} from "@ant-design/icons";
+import Meta from "antd/es/card/Meta";
 
 function Home(props) {
+
+    const data = [
+        'Racing car sprays burning fuel into crowd.',
+        'Japanese princess to wed commoner.',
+        'Australian walks 100km after outback crash.',
+        'Man charged over missing wedding girl.',
+        'Los Angeles battles huge wildfires.',
+    ];
+
     return (
-        <div>
-            Home
+        <div className="site-card-wrapper">
+            <Row gutter={16}>
+                <Col span={8}>
+                    <Card title="用户最常浏览" bordered={true}>
+                        <List
+                            size="small"
+                            dataSource={data}
+                            renderItem={(item) => <List.Item>{item}</List.Item>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card title="用户点赞最多" bordered={true}>
+                        <List
+                            size="small"
+                            dataSource={data}
+                            renderItem={(item) => <List.Item>{item}</List.Item>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card
+                        cover={
+                            <img
+                                alt="example"
+                                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                            />
+                        }
+                        actions={[
+                            <SettingOutlined key="setting"/>,
+                            <EditOutlined key="edit"/>,
+                            <EllipsisOutlined key="ellipsis"/>,
+                        ]}
+                    >
+                        <Meta
+                            avatar={<Avatar src="https://joeschmoe.io/api/v1/random"/>}
+                            title="Card title"
+                            description="This is the description"
+                        />
+                    </Card>
+                </Col>
+            </Row>
         </div>
     );
 }
